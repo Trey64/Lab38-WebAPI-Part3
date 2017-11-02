f521e63ad37bfd163262e605b7e4e5fc30739735
Thomas Reyes <thomasellison.r@gmail.com>
2017-11-02 11:30:08 -0700

    dependency injection

--- Week8Tom/Week8Tom/Controllers/PowersController.cs
+++ Week8Tom/Week8Tom/Controllers/PowersController.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Week8Tom.Data;
 
 namespace Week8Tom.Controllers
 {
     public class PowersController
     {
+        private readonly HeroStatsDbContext _context;
+        //Dependency injection of context
+        public PowersController(HeroStatsDbContext context)
+        {
+            _context = context;
+        }
     }
 }
